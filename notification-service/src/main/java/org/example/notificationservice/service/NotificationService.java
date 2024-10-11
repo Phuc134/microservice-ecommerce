a9ed9c693d8d3e9984c07b7c7e24f75807982e17
phuc <phuc@vnpt.vn>
2024-10-12 04:27:08 +0700

    commit

--- notification-service/src/main/java/org/example/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/org/example/notificationservice/service/NotificationService.java
@@ -10,6 +10,7 @@ import org.example.notificationservice.dto.request.Recipient;
 import org.example.notificationservice.dto.request.SendEmailRequest;
 import org.example.notificationservice.dto.request.Sender;
 import org.example.notificationservice.httpClient.EmailClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +22,8 @@ import java.util.Map;
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class NotificationService {
     EmailClient emailClient;
-    String apiKey = "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";
-
+    @Value("${apiKey.email}")
+    String apiKey;
     public String sendEmail(SendEmailRequest request) {
         Sender sender = Sender.builder()
                 .name("phuc")
